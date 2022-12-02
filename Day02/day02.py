f1d8ddf2dfcad204fb9c5d03a16967b72c16fe63
Darrell <dchinon@yahoo.com>
2022-12-02 14:33:28 +0100

    finished part 2

--- Day02/day02.py
+++ Day02/day02.py
@@ -1,3 +1,37 @@
+# X = lose
+# Y = draw
+# Z = win
+# A Y  
+# B X
+# C Z
+
+def newoutcome(player1, result):
+    score = 0    
+    # Need to be a draw
+    if player1.lower() == 'a' and result.lower() == 'y':
+        score += 3 + 1
+    if player1.lower() == 'b' and result.lower() == 'y':
+        score += 3 + 2
+    if player1.lower() == 'c' and result.lower() == 'y':
+        score += 3 + 3
+
+    # need to lose
+    if player1.lower() == 'a' and result.lower() == 'x':
+        score += 0 + 3
+    if player1.lower() == 'b' and result.lower() == 'x':
+        score += 0 + 1
+    if player1.lower() == 'c' and result.lower() == 'x':
+        score += 0 + 2
+
+    # need to win
+    if player1.lower() == 'a' and result.lower() == 'z':
+        score += 6 + 2
+    if player1.lower() == 'b' and result.lower() == 'z':
+        score += 6 + 3
+    if player1.lower() == 'c' and result.lower() == 'z':
+        score += 6 + 1
+    return score
+
 def outcome(player1, player2):
     score = 0    
     # player 2 losed
@@ -23,16 +57,16 @@ def outcome(player1, player2):
         score += 3 + 2
     if player1.lower() == 'c' and player2.lower() == 'z':
         score += 3 + 3
-
     return score
     
 file = open("input.txt", "r")
 lines = file.readlines()
-total = 0
-count = 0
+total_part1 = 0
+total_part2 = 0
 
 for line in lines:
-    count += 1
-    total += outcome(line[0], line[2])
+    total_part1 += outcome(line[0], line[2])
+    total_part2 += newoutcome(line[0], line[2])
 
-print (f'{total}')
+print (f'part 1: {total_part1}')
+print (f'part 2: {total_part2}')
