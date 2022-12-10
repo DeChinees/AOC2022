2a413275c6769e9456952950e4fa0b6142bec6c2
Darrell ChinOn <dchinon@yahoo.com>
2022-12-10 14:17:07 +0100

    not yet moving up the array

--- Day06/day-06-part-01.py
+++ Day06/day-06-part-01.py
@@ -7,15 +7,22 @@ with open('Day06/test.txt', 'r') as file:
     line = file.read()
 
 stream = list("bvwbjplbgvbhsrlpgdmjqwftvncz")
-start = stream[0:]
-print(f"{stream}")
-print(f"First 3 chars: {start}")
+streamlenth = len(stream)
+s_pos = 0
+e_pos = 3
+block = ""
 
+#print(f"{stream}")
+print(f"{stream[0:2]}")
+#print(f"First 3 chars: {start}")
 
-for c in stream[1:]:
-    if (c not in start):
-        print(f"")
-        start.append(c)
-    if start:
-        if (c == "j"):
-            print(f"J found at index: {stream.index(c)}")
+while s_pos < streamlenth -1:
+    print(f"s_pos: {stream[s_pos:e_pos:]}")
+    block = stream[s_pos:e_pos:]
+    if not (set(stream[e_pos+1]).issubset(set(block))):
+        print(f"at index {e_pos+1} the char {stream[e_pos+1]} is subset of {block}")
+    else:
+        s_pos += 1          # we move block one step to right
+        e_pos = s_pos+3
+        block = stream[s_pos:e_pos:]
+    
