29a4c7ec3790adc3034548dcf4bf4134a12d9c8e
Darrell ChinOn (ext.) <darrell.chin-on@partner.bmwgroup.com>
2022-12-12 15:01:42 +0100

    fixed with help of Vincent

--- Day06/day-06-part-01.py
+++ Day06/day-06-part-01.py
@@ -7,44 +7,26 @@ with open('Day06/input.txt', 'r') as file:
     line = file.read()
 
     
-str="mjqjpqmgbljsphdztnvjfqwrcgsmlb"
-stream = list("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+stream = list(line)
 streamlenth = len(stream)
 window_size = 4
-# e_pos = 3
-# block = ""
-
-# print(f"{stream}")
-# #print(f"{stream[0:2]}")
-# #print(f"First 3 chars: {start}")
-
-# print(f"Starting at {stream[s_pos:e_pos]}")
-# block = stream[s_pos:e_pos]
 
 
+def dubcheck(block):
+    hist=""
+    for c in (block):
+        if c in hist: 
+            return False
+        hist += c
+    print(f"hist: {hist}")
+    return True
+        
 
 if len(stream) <= window_size:
     print(f"{stream}")
 for i in range(len(stream)):
     print(stream[i:i+window_size])
+    if dubcheck(stream[i:i+window_size]):
+        print (f"{i+window_size}")
+        break
     
-
-        
-
-# while s_pos < streamlenth -1:
-#     print(F"e_pos: {stream[e_pos]}")
-#     if (set(stream[e_pos]).issubset(set(block))):
-#         print(f"at index {e_pos} the char {stream[e_pos]} is in subset of {block}")
-#         s_pos=e_pos
-#         e_pos = s_pos+3
-#         block = stream[s_pos:e_pos]
-#         print(f"new block: {block}")
-#     else:
-#         if not (set(stream[e_pos]).issubset(set(block))):
-#             print(f"at index {stream.index(stream[e_pos])} the char {stream[e_pos]} is in NOT subset of {block}")
-#             s_pos += 1          # we move block one step to right
-#             e_pos = s_pos+3
-#             block = stream[s_pos:e_pos]
-        
-#     if e_pos+1 >= streamlenth: 
-#         break
