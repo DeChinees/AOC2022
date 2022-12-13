b9f5e649217ff20d7cc74ca4f3c3ae2f334abf95
Darrell ChinOn (ext.) <darrell.chin-on@partner.bmwgroup.com>
2022-12-13 15:18:43 +0100

    first try

---
+++ Day07/day-07-part-01.py
@@ -0,0 +1,37 @@
+import pathlib
+
+dir_size = {}
+dir_stack = []
+
+input = pathlib.Path('Day07/test.txt').read_text().split("\n")
+print(f"{input}")
+
+for i in input:
+    match i.split(" "):
+        case '$', 'cd', directory:
+            if directory == '..':
+                dir_stack.pop()
+            else:
+                dir_stack.append(directory)
+        case '$', 'ls':
+            continue
+        case 'dir', _:
+            continue
+        case size, _:
+            for idx in range(len(dir_stack)):
+                curr_path = '/' if idx == 0 else '/'.join(dir_stack[:idx+1])[1:]
+            if curr_path not in dir_size:
+                dir_size[curr_path] = 0
+            dir_size[curr_path] += int(size)
+        case other:
+            continue        
+
+s=0
+print(f"dir_stack:{dir_size}")
+#s = sum([size for size in dir_size.values() if size <= 100000])
+for size in dir_size.values():
+    if size <= 100000:
+        print(f"dir_size:{size}")
+        s += size
+print(f"total:{s}")
+#print(f"dir_stack:{dir_stack}")
