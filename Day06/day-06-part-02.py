9a196c3d13ca5121acfa53224bad98a33cb86624
Darrell ChinOn (ext.) <darrell.chin-on@partner.bmwgroup.com>
2022-12-13 08:22:07 +0100

    works

---
+++ Day06/day-06-part-02.py
@@ -0,0 +1,27 @@
+with open('Day06/input.txt', 'r') as file:
+    line = file.read()
+
+    
+stream = list(line)
+streamlenth = len(stream)
+window_size = 14
+
+
+def dubcheck(block):
+    hist=""
+    for c in (block):
+        if c in hist: 
+            return False
+        hist += c
+    print(f"hist: {hist}")
+    return True
+        
+
+if len(stream) <= window_size:
+    print(f"{stream}")
+for i in range(len(stream)):
+    print(stream[i:i+window_size])
+    if dubcheck(stream[i:i+window_size]):
+        print (f"{i+window_size}")
+        break
+    
